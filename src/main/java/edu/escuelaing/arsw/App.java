368966a761574bac877bb59a3510798df55aa883
Maria Camila Fetecua <maria.fetecua@mail.escuelaing.edu.co>
2021-06-02 15:49:32 -0500

    Terminado LICENSE , pom y .gitinore

---
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -0,0 +1,18 @@
+package edu.escuelaing.arsw;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class App{
+    public static void main (String[] args){
+        String typeOfCount= args[0];
+        String fileName = args[1];
+    }
+    if(typeOfCount.equals("phy")){
+        // Escriba las lineas fisicas del codigo
+    }else if(typeOfCount.equals("loc")){
+        //Escriba las lineas de codigo encontradas
+
+    }
+}
